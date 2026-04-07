396cde988e1cbc9d0cd5ec34988ec02e129b11a8
tushar-404 <tushar459k@gmail.com>
2026-04-07 19:31:36 +0530

    some cleanup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,78 +1,19 @@
-"use client"
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { authClient } from '@/lib/auth-client';
-import { Loader } from 'lucide-react';
-import { useTransition } from 'react';
-// import { GithubIcon } from 'lucide-react';
-import { FaGithub } from 'react-icons/fa';
-import { toast } from 'sonner';
 
-export default function LoginPage() {
-  const [githubPending, startGithubTransition] = useTransition()
+import { auth } from "@/lib/auth";
+import LoginForm from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-  async function signInWithGithub () {
-    startGithubTransition(async () => {
-      await authClient.signIn.social({
-      provider: 'github',
-      callbackURL: '/',
-      fetchOptions: {
-        onSuccess: () => {
-          toast.success("Signed in with Github, you will be redirected")
-        }, 
-        onError: (error) => {
-          toast.error(error.error.message)
-        } 
-      }
-    })
-    })
+export default async function Login() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if(session) {
+    return redirect("/");
   }
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login with your Github or Email</CardDescription>
-      </CardHeader>
-      <CardContent className='flex flex-col gap-3'>
-        <Button
-        disabled={githubPending} 
-        onClick={signInWithGithub}
-        className='w-full cursor-pointer'
-        variant="outline">
-          {githubPending ? (
-            <>
-             <Loader className='size-4 animate-spin'/>
-             <span>Loading.... </span>
-            </>
-          ): (
-            <>
-              <FaGithub className='size-4'/>
-              Sign in with Github
-            </>
-          )}
-        </Button>
-        <div className='relative text-center text-sm after:absolute
-        after:top-1/2 after:inset-0 after:z-0 after:flex after:items-center after:border-t after:border-border '>
-          <span className='relative z-10 bg-card px-2 text-muted-foreground'>Or continue with</span>
-        </div>
-        <div className='grid gap-3'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input  
-            type='email'
-            placeholder='m@example.com'/>
-          </div>
-          <Button className='cursor-pointer'>Continue with Email</Button>
-        </div>
-      </CardContent>
-    </Card>
-  );
+
+  return(
+    <LoginForm />
+  )
 }
