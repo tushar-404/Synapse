4ee9ba634d25c1969f15ae3f083db33ff909d9de
tushar-404 <tushar459k@gmail.com>
2026-04-22 16:17:25 +0530

    updated schema

---
+++ app/admin/courses/page.tsx
@@ -0,0 +1,22 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function CoursesPage(){
+    return(
+        <>
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-bold ">
+                    Your Courses
+                </h1>
+                <Link href={"/admin/courses/create"}
+                className={buttonVariants()}>
+                    Create Courses
+                </Link>
+            </div>
+
+            <div>
+                Here You wil see all the courses
+            </div>
+        </>
+    )
+}
