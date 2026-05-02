5e92529bdf2da73a7cee8249f8779852ca7d2c53
tushar-404 <tushar459k@gmail.com>
2026-05-02 15:45:58 +0530

    rate limit

--- app/api/s3/delete/route.ts
+++ app/api/s3/delete/route.ts
@@ -1,10 +1,36 @@
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
 import { env } from "@/lib/env";
 import { S3 } from "@/lib/S3Client";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+const aj = arcjet.withRule(
+    detectBot({
+        mode: "LIVE",
+        allow:[]
+    }),
+).withRule(
+    fixedWindow({
+        mode: "LIVE",
+        window: "1m",
+        max: 5
+    })
+)
 
 export async function DELETE(request: Request){
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
     try {
+        const decision = await aj.protect(request, {
+            fingerprint: session?.user.id as string,
+        })
+
+        if(decision.isDenied()) {
+            return NextResponse.json({error: "😠😠😠😠😠😠😠"}, {status: 429})
+        }
+
         const body = await request.json();
 
         const key = body.key
--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -5,6 +5,10 @@ import { z } from 'zod'
 import { v4 as uuidv4 } from "uuid";
 import { S3 } from '@/lib/S3Client';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+
 
 export const fileUploadSchema = z.object({
     fileName: z.string().min(1, {message: "Filename is required"}),
@@ -13,9 +17,32 @@ export const fileUploadSchema = z.object({
     isImage: z.boolean()
 })
 
+const aj = arcjet.withRule(
+    detectBot({
+        mode: "LIVE",
+        allow:[]
+    }),
+).withRule(
+    fixedWindow({
+        mode: "LIVE",
+        window: "1m",
+        max: 5
+    })
+)
 
 export async function POST(request: Request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
     try {
+
+        const decision = await aj.protect(request, {
+            fingerprint: session?.user.id as string
+        })
+
+        if(decision.isDenied()) {
+            return NextResponse.json({error: "😠😠😠😠😠😠😠😠😠😠"}, {status: 429})
+        }
         const body = await request.json();
         const validation = fileUploadSchema.safeParse(body)
         if(!validation.success) {
